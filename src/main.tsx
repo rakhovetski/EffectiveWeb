25a384f80b93b9b51b91f82f37baa5c15f40ac3b
Andrey Rakhovetki <rakhovetski@mail.ru>
2023-12-15 08:58:55 +0600

    Update fonts

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import './fonts/Mulish/Mulish-VariableFont_wght.ttf'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
